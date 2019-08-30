f6697958ec359f2390b7472628d23e036be44b1e
edeediong <edidiongmichael08@gmail.com>
2019-08-30 14:08:13 +0100

    removed pycache

--- bookModel.py
+++ bookModel.py
@@ -12,3 +12,22 @@ class Book(db.Model):
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
     isbn = db.Column(db.Integer, nullable=False)
+
+    def add_book(_name, _price, _isbn):
+        # add book to database
+        new_book = Book(name=_name, price=_price, isbn=_isbn)
+        db.session.add(new_book)
+        db.session.commit()
+
+    def get_all_books():
+        # fetches all booksin database
+        return Book.query.all()
+
+    def __repr__(self):
+        # define how the output will look like
+        book_object = {
+            'name': self.name,
+            'price': self.price,
+            'isbn': self.isbn
+        }
+        return json.dumps(book_object)
