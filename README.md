e8fdf8b72fb1b49dfc50010ac3c5095de3b7347c
edeediong <edidiongmichael08@gmail.com>
2019-08-30 13:51:20 +0100

    no errors

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This app.py was built using Flask framework.
 
 It is based on the PluralSight course, Building RestFul APIs with Flask.
 
+I had issues with the directory path,so I had to use os library
+
 Found at https://app.pluralsight.com/library/courses/python-flask-rest-api/table-of-contents 
--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+targetdir = os.path.join(basedir, 'database.db')
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqite:///Users/Edeediong/Documents/Codes/database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + targetdir
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
