1d9f2d845ad233feb59ac905747b08a9e38cb87c
edeediong <edidiongmichael08@gmail.com>
2019-08-30 15:11:23 +0100

    new functions

--- bookModel.py
+++ bookModel.py
@@ -23,6 +23,29 @@ class Book(db.Model):
         # fetches all booksin database
         return Book.query.all()
 
+    def get_book(_isbn):
+        return Book.query.filter_by(isbn=_isbn).first()
+
+    def delete_book(_isbn):
+        Book.query.filter_by(isbn=_isbn).delete()
+        db.session.commit()
+
+    def update_book_price(_isbn, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.price = _price
+        db.session.commit()
+
+    def update_book_name(_isbn, _name):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.name = _name
+        db.session.commit()
+
+    def replace_book(_isbn, _name, _price):
+        book_to_replace = Book.query.filter_by(isbn=_isbn).first()
+        book_to_replace.name = _name
+        book_to_replace.price = _price
+        db.session.commit()
+
     def __repr__(self):
         # define how the output will look like
         book_object = {
